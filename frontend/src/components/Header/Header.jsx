362ac010cc42babe91898bbee5cad7189ec28c80
sunny kumar <kumarsunny16082002@gmail.com>
2024-06-22 18:46:39 +0530

    first commit

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+function Header() {
+  return (
+    <div className='header'>
+      <div className="header-contents">
+        <h2>order your favourite food here</h2>
+        <p>Choose from a diverse menu featuring a deluctable array of dishes crafted with the </p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
